d5ecebe547af00a405e48a4d18fe951caaffbc22
modest0w <56058578+modest0w@users.noreply.github.com>
2019-11-01 23:44:04 -0300

    modesto

--- src/Server/FileServer.java
+++ src/Server/FileServer.java
@@ -2,34 +2,61 @@ package Server;
 
 import java.util.ArrayList;
 
+/**
+ * Classe modelo de arquivo do servidor.
+ * Contem todos os atributos e metodos diretamente relacionados aos arquivos manipulados pelo servidor
+ */
 public class FileServer {
-    private String name;
-    private String directory;
-    private ArrayList<Client> clients;
-    private long size;
-    private String modificationDate;
-    private String creator;
+    private String name; //nome do arquivo
+    private String directory;  //diretorio do arquivo
+    private ArrayList<Client> clients; //lista de clientes conectados
+    private long size; //tamanho do arquivo
+    private String modificationDate; //data de modificação
+    private String creator; //nome do criador do arquivo
 
+    /**
+     * Inicializa um FileServer de tamanho 0
+     */
     public FileServer(){
         this.size = 0;
     }
 
+    /**
+     * seta a string com o nome do arquivo
+     * @param name - string com nome do arquivo
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * seta a string do diretorio do arquivo
+     * @param directory - string do diretorio do arquivo
+     */
     public void setDirectory(String directory) {
         this.directory = directory;
     }
 
+    /**
+     * seta a lista dos clientes conectados
+     * @param clients ArrayList de clients
+     */
     public void setClients(ArrayList<Client> clients) {
         this.clients = clients;
     }
 
+    /**
+     * seta o tamanho do arquivo
+     * @param size long do tamanho do arquivo
+     */
     public void setSize(long size) {
         this.size = size;
     }
 
+    /**
+     * seta a data de modificação do arquivo
+     * @param modificationDate string
+     */
     public void setModificationDate(String modificationDate) {
         this.modificationDate = modificationDate;
     }
